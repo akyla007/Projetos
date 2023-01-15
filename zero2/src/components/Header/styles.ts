79ac0803126a65b9c1b11e52a631879cfbaef78b
Akyla <akylaaquino@hotmail.com>
2023-01-15 12:24:46 -0300

    Fazendo Header e Body

---
+++ zero2/src/components/Header/styles.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  img {
+    width: 40px;
+    height: 40px;
+    filter: invert(40%) sepia(88%) saturate(829%) hue-rotate(125deg)
+      brightness(102%) contrast(101%);
+  }
+
+  nav {
+    display: flex;
+    gap: 0.5rem;
+  }
+
+  a {
+    width: 3rem;
+    height: 3rem;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    color: ${(props) => props.theme['gray-100']};
+
+    border-top: 3px solid transparent;
+    border-bottom: 3px solid transparent;
+
+    &:hover {
+      border-bottom: 3px solid ${(props) => props.theme['green-500']};
+    }
+
+    &.active {
+      color: ${(props) => props.theme['green-500']};
+    }
+  }
+`
